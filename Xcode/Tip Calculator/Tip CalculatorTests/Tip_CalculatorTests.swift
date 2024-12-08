e03c66760ea7f109e60199af920b24de3b1cac98
felderelijah <112753774+felderelijah@users.noreply.github.com>
2024-12-08 02:19:47 -0500

    Add files via upload

---
+++ Xcode/Tip Calculator/Tip CalculatorTests/Tip_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorTests.swift
+//  Tip CalculatorTests
+//
+//  Created by Elijah Felder on 12/7/24.
+//
+
+import Testing
+@testable import Tip_Calculator
+
+struct Tip_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
