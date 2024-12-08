f9261c8306cf4b49d5c717175c0c320cbd2183ab
felderelijah <112753774+felderelijah@users.noreply.github.com>
2024-12-07 19:43:58 -0500

    Add files via upload

---
+++ Xcode/My Favorite Games/My Favorite GamesTests/My_Favorite_GamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Favorite_GamesTests.swift
+//  My Favorite GamesTests
+//
+//  Created by Elijah Felder on 12/6/24.
+//
+
+import Testing
+@testable import My_Favorite_Games
+
+struct My_Favorite_GamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
