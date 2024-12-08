6cad96a49b7fdd35853a8efc4a843fb7b3d48819
felderelijah <112753774+felderelijah@users.noreply.github.com>
2024-12-07 19:48:03 -0500

    Add files via upload

---
+++ Xcode/My Favorite Place/My Favorite PlaceTests/My_Favorite_PlaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Favorite_PlaceTests.swift
+//  My Favorite PlaceTests
+//
+//  Created by Elijah Felder on 11/13/24.
+//
+
+import Testing
+@testable import My_Favorite_Place
+
+struct My_Favorite_PlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
